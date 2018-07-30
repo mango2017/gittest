b8e8259fb4a91306b74659b113f5ba635ff33dc2
jly <1210056515@qq.com>
2018-07-30 23:11:19 +0800

    second_branch

--- 1.php
+++ 1.php
@@ -5,8 +5,4 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo "this is test branch";
-echo "coding.....";
-echo "add";
-
-echo "test code";
+echo "second_branch";
